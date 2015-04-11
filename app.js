faedae1688c48e3eea327a3b4d65d4fa4d3e36fa
Adrian Chen <adrianchen@rgnt1-59-35-dhcp.int.colorado.edu>
2015-04-10 21:55:29 -0600

    basic UI setup

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
+// use jade as the view engine
+app.set('view engine', 'jade');
+
+// set where the static contents are (e.g., css, js)
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index.jade')
 });
 
-var server = app.listen(3000, function () {
+app.set('port', (process.env.PORT || 3000))
 
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(app.get('port'), function() {
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+    var host = server.address().address
+    var port = server.address().port
+    console.log('App listening at http://%s:%s', host, port)
+})

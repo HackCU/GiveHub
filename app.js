a3567cb41138a4f93085ea1d8dcfe62e668b4d46
Brian Newsom <brian.newsom@colorado.edu>
2015-04-11 12:51:30 -0600

    Braintree page

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.get('/createOrganization', function(req, res){
 app.get('/braintree', function(req, res){
   res.render('braintree.jade',
     {
-      "organization" : "Example Company",
-      "amount" : "$10"
+      "organization" : req.query.handle || "Example",
+      "amount" : req.query.amount || "$10"
     }
   );
 });

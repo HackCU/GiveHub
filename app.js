eec6edad0a8d29bb6a5667b90f14664d684b79d3
BrianNewsom <brian.newsom@colorado.edu>
2015-04-10 22:47:06 -0600

    Add data, filter twitter for hashtag and handle

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/twitter', function(req,res){
+    var stream = require('./controllers/twitter')();
+});
+
 
 var server = app.listen(3000, function () {
 
